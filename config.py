e014a2641457124cd85705cd24397f5704702f34
Jeenesh Raja <eljeyaar@gmail.com>
2026-05-07 13:41:07 +0530

    added flask app for render

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
-import os
-
 # ====== CONFIG ======
-API_KEY = os.getenv("API_KEY")
-API_SECRET = os.getenv("API_SECRET")
-SUPABASE_DSN = os.getenv("SUPABASE_DSN")
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+API_KEY = "hqszgobglsa0drch"
+API_SECRET = "8xnttgisqub0vbb6j5xxdd7wkkczk66u"
+PORT = 8000  # local server port
+TRADINGSYMBOL = "GOLDTEN26MAYFUT"  # MCX GoldTen May 26 future
 
+TELEGRAM_BOT_TOKEN = "8629379589:AAEeYlqkqWvDnvGh__TLyUlk4qSL2QP_wYo"
+TELEGRAM_CHAT_ID = "1200810241"
+
+# ====== SUPABASE CONFIG ======
+SUPABASE_DSN = "postgresql://postgres.nqxtoltiqyeouqcndqfb:MFT_CTT_123$@aws-1-ap-southeast-2.pooler.supabase.com:5432/postgres"
+
+# ====== Trading Symbol ======
+TRADINGSYMBOL = "GOLDTEN26MAYFUT"
+SILVERMIC_TRADINGSYMBOL = "SILVERMIC30JUNFUT"
+
+# Local server port
 PORT = 8000
